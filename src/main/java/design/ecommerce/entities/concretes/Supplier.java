fa9b4150e52f94fa62febdea8c493bb5574b7bb2
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-12-22 00:22:32 +0300

    Entities.concrete - Supplier created

---
+++ src/main/java/design/ecommerce/entities/concretes/Supplier.java
@@ -0,0 +1,57 @@
+package design.ecommerce.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "suppliers")
+public class Supplier {
+    @Id
+    @Column(name = "supplier_id")
+    private int supplierId;
+
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "contact_name")
+    private String contactName;
+
+    @Column(name = "contact_title")
+    private String contactTitle;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "region")
+    private String region;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "fax")
+    private String fax;
+
+    @Column(name = "homepage")
+    private String homepage;
+
+    @OneToMany(mappedBy = "supplier")
+    private List<Product> products;
+}
