589ce2374d6bcef3e87c9e2ec48dd29c448a77cf
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-12-22 02:52:26 +0300

    Inside core package - SuccessDataResult and ErrorDataResult class were created.

---
+++ src/main/java/design/ecommerce/core/utilities/results/dataResult/error/ErrorDataResult.java
@@ -0,0 +1,22 @@
+package design.ecommerce.core.utilities.results.dataResult.error;
+
+import design.ecommerce.core.utilities.results.dataResult.DataResult;
+
+public class ErrorDataResult<T> extends DataResult {
+
+    public ErrorDataResult(String message, T data) {
+        super(false, message, data);
+    }
+
+    public ErrorDataResult(T data){
+        super(false, data);
+    }
+
+    public ErrorDataResult(String message){
+        super(false, message, null);
+    }
+
+    public ErrorDataResult(){
+        super(false, null);
+    }
+}
---
+++ src/main/java/design/ecommerce/core/utilities/results/dataResult/success/SuccessDataResult.java
@@ -0,0 +1,21 @@
+package design.ecommerce.core.utilities.results.dataResult.success;
+
+import design.ecommerce.core.utilities.results.dataResult.DataResult;
+
+public class SuccessDataResult<T> extends DataResult {
+    public SuccessDataResult(String message, T data) {
+        super(true, message, data);
+    }
+
+    public SuccessDataResult(T data){
+        super(true, data);
+    }
+
+    public SuccessDataResult(String message){
+        super(true, message, null);
+    }
+
+    public SuccessDataResult(){
+        super(true, null);
+    }
+}
