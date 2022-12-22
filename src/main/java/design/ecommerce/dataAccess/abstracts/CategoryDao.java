d6aeb8fc3ee253e0c90fe391c8f71ba30c5939c0
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-12-22 06:45:00 +0300

    Removed unnecessary functions in CategoryDao

--- src/main/java/design/ecommerce/dataAccess/abstracts/CategoryDao.java
+++ src/main/java/design/ecommerce/dataAccess/abstracts/CategoryDao.java
@@ -6,14 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CategoryDao extends JpaRepository<Category, Integer> {
-    Category getByCategoryId(int categoryId);
     Category getByCategoryName(String categoryName);
-    Category getByDescription(String description);
-
-    Category getByCategoryIdAndCategoryName(int categoryId, String categoryName);
-    Category getByCategoryNameAndDescription(String categoryName, String description);
-    Category getByCategoryIdAndCategoryNameAndDescription(int categoryId, String description);
-
     List<Category> getByCategoryNameContains(String categoryName);
+
+    List<Category> getByDescriptionContains(String description);
+
+    List<Category> getByCategoryNameContainsOrDescriptionContains(String categoryName, String description);
+
     List<Category> getByCategoryNameStartingWith(String categoryName);
 }
