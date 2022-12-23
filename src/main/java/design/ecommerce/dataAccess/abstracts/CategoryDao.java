c33cad3f5d80e6cb4e669502e1fc1f9f08617404
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-12-23 06:11:17 +0300

    Added "IgnoreCase" query detail to some functions

--- src/main/java/design/ecommerce/dataAccess/abstracts/CategoryDao.java
+++ src/main/java/design/ecommerce/dataAccess/abstracts/CategoryDao.java
@@ -6,12 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CategoryDao extends JpaRepository<Category, Integer> {
-    Category getByCategoryName(String categoryName);
-    List<Category> getByCategoryNameContains(String categoryName);
+    Category getByCategoryNameIgnoreCase(String categoryName);
+    List<Category> getByCategoryNameContainsIgnoreCase(String categoryName);
 
     List<Category> getByDescriptionContains(String description);
 
-    List<Category> getByCategoryNameContainsOrDescriptionContains(String categoryName, String description);
+    List<Category> getByCategoryNameContainsIgnoreCaseOrDescriptionContainsIgnoreCase(String categoryName, String description);
 
     List<Category> getByCategoryNameStartingWith(String categoryName);
 }
