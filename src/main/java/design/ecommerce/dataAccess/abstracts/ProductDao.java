31ac9ac47ba4ada236c30510294646563c3cd43e
RafetErsoy <rafet.ersoy.cs@gmail.com>
2022-12-23 05:32:13 +0300

    Functions edited.

--- src/main/java/design/ecommerce/dataAccess/abstracts/ProductDao.java
+++ src/main/java/design/ecommerce/dataAccess/abstracts/ProductDao.java
@@ -2,6 +2,7 @@ package design.ecommerce.dataAccess.abstracts;
 
 import design.ecommerce.entities.concretes.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -11,14 +12,12 @@ public interface ProductDao extends JpaRepository<Product, Integer> {
 
     Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
 
-    List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
+    List<Product> getByProductNameContainsIgnoreCaseAndCategory_CategoryId(String productName, int categoryId);
+    List<Product> getByCategory_CategoryId(int categoryId);
 
-    List<Product> getByCategoryIn(List<Integer> categories);
+    List<Product> getByCategory_CategoryIdIn(List<Integer> categories);
 
-    List<Product> getByProductNameContains(String productName);
+    List<Product> getByProductNameContainsIgnoreCase(String productName);
 
-    List<Product> getByProductNameStartsWith(String productName);
-
-    @Query("From Product where productName=:productName and category.categoryId=:categoryId")
-    List<Product> getByNameAndCategory(String productName, int categoryId);
+    List<Product> getByProductNameStartsWithIgnoreCase(String productName);
 }
